c958db462e8517f74b44cbe505b05ef7d80b7b48
dongduu <4012popo@naver.com>
2021-12-16 00:12:43 +0900

    feat: practice RegExp

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(11)
+const str = `
+010-1234-5678
+4012popo@naver.com
+https://www.velog/dongduu
+hi i'm dongduu
+abbcccdddd
+`
+
+//const regexp = new RegExp('o','g')
+const regexp = /o/gi
+console.log(str.match(regexp))
