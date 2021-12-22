0de9e564b2dcde35b446620824aa67dbc38e1278
dongduu <4012popo@naver.com>
2021-12-22 14:38:26 +0900

    feat: practice regexp pattern

--- main.js
+++ main.js
@@ -2,11 +2,21 @@ const str = `
 010-1234-5678
 4012popo@naver.com
 https://www.velog/dongduu
-hi i'm dongduu
+hi i'm dongduu, DongDuu.
+http://www.naver.com
 abbcccdddd
+hxyp
 `
 
-const regexp = /dongduu/gi
-console.log(regexp.test(str)) // true 출력
-console.log(str.replace(regexp, 'dongju'))
-console.log(str)
+console.log(
+    str.match(/./g)
+    //str.match(/h..p/g)
+)
+console.log(str.match(/4012|popo/g))
+console.log(str.match(/https?/g))
+console.log(str.match(/d{2}/))
+console.log(str.match(/d{2,}/))
+console.log(str.match(/d{2,3}/))
+console.log(str.match(/d{2}/))
+console.log(str.match(/d{2,}/))
+console.log(str.match(/\b\w{2,3}\b/g))
